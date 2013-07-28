5e7e4d1e35b7cd3738371f00707dc494c6408eb0
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-07-28 11:05:10 +0200

    Simplifying exception handling

--- src/se/vidstige/android/uimultimator/UiAutomatorRunner.java
+++ src/se/vidstige/android/uimultimator/UiAutomatorRunner.java
@@ -1,6 +1,6 @@
 package se.vidstige.android.uimultimator;
 
-import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,27 +15,34 @@ class UiAutomatorRunner {
 		adb = new AdbDevice(serial);
 	}
 
-	public void sendRaw(String ... arguments) throws IOException, InterruptedException, UiMultimatorException {
+	public void sendRaw(String ... arguments) throws UiMultimatorException {
 		adb.sendAdbCommand(new DummyParser(), arguments);		
 	}
 	
-	public void run(String classname, String methodname) throws IOException, InterruptedException, UiMultimatorException
+	public void run(String classname, String methodname) throws UiMultimatorException
 	{
 		adb.sendAdbCommand(new ReadTestParser(classname, methodname), "shell", "uiautomator", "runtest", jarfile,
 				"-c", classname + "#" + methodname);
 	}
 
-	public String run(String classname, String methodname, Map<String, String> parameters) throws IOException, InterruptedException, UiMultimatorException {
-		ArrayList<String> arguments = new ArrayList<String>(Arrays.asList("shell", "uiautomator", "runtest", jarfile,
-				"-c", classname + "#" + methodname));
-		for (Entry<String, String> entry : parameters.entrySet())
+	public String run(String classname, String methodname, Map<String, String> parameters) throws UiMultimatorException {
+		try
 		{
-			arguments.add("-e");
-			arguments.add(entry.getKey());
-			arguments.add(URLEncoder.encode(entry.getValue(), "utf-8"));
+			ArrayList<String> arguments = new ArrayList<String>(Arrays.asList("shell", "uiautomator", "runtest", jarfile,
+					"-c", classname + "#" + methodname));
+			for (Entry<String, String> entry : parameters.entrySet())
+			{
+				arguments.add("-e");
+				arguments.add(entry.getKey());
+				arguments.add(URLEncoder.encode(entry.getValue(), "utf-8"));
+			}
+			ReadTestParser parser = new ReadTestParser(classname, methodname);
+			adb.sendAdbCommand(parser, arguments);
+			return parser.getResponse();
 		}
-		ReadTestParser parser = new ReadTestParser(classname, methodname);
-		adb.sendAdbCommand(parser, arguments);
-		return parser.getResponse();
+		catch (UnsupportedEncodingException  e)
+		{
+			throw new UiMultimatorException("Could not run test on device", e);
+		}
 	}
 }
--- src/se/vidstige/android/uimultimator/UiDevice.java
+++ src/se/vidstige/android/uimultimator/UiDevice.java
@@ -1,39 +1,27 @@
 package se.vidstige.android.uimultimator;
 
-import java.io.IOException;
 
 public class UiDevice {
 
 	private final UiAutomatorRunner runner;
 	
 	UiDevice(String serial) throws UiMultimatorException {
-		try
-		{
-			runner = new UiAutomatorRunner(serial);
-			
-	//		File tmpFile = File.createTempFile("uimultimator-command-tests", ".jar");
-	//		OutputStream out = new FileOutputStream(tmpFile);
-	//		InputStream in = getClass().getResourceAsStream("command-tests/bin/command-tests.jar");
-	//		
-	//		byte[] buf = new byte[1024];		
-	//		int len;
-	//		while ((len = in.read(buf)) >= 0) {
-	//	        out.write(buf, 0, len);
-	//		}
-	//		in.close();
-	//		out.close();
-			
-			String deluxJar = System.getProperty("user.dir") + "/command-tests/bin/command-tests.jar";
-			runner.sendRaw("push", deluxJar, "/data/local/tmp/");
-		}
-		catch (IOException e)
-		{
-			throw new UiMultimatorException("Could not create UiDevice", e);
-		}
-		catch (InterruptedException e)
-		{
-			throw new UiMultimatorException("Could not create UiDevice", e);
-		}
+		runner = new UiAutomatorRunner(serial);
+		
+//		File tmpFile = File.createTempFile("uimultimator-command-tests", ".jar");
+//		OutputStream out = new FileOutputStream(tmpFile);
+//		InputStream in = getClass().getResourceAsStream("command-tests/bin/command-tests.jar");
+//		
+//		byte[] buf = new byte[1024];		
+//		int len;
+//		while ((len = in.read(buf)) >= 0) {
+//	        out.write(buf, 0, len);
+//		}
+//		in.close();
+//		out.close();
+		
+		String deluxJar = System.getProperty("user.dir") + "/command-tests/bin/command-tests.jar";
+		runner.sendRaw("push", deluxJar, "/data/local/tmp/");
 	}
 
 	public UiObject newUiObject(UiSelector selector) {
@@ -50,17 +38,6 @@ public class UiDevice {
 	
 	private void runTest(String methodname) throws UiMultimatorException
 	{
-		try
-		{
-			runner.run("se.vidstige.android.uimultimator.UiDeviceCommands", methodname);
-		}
-		catch (IOException e)
-		{
-			throw new UiMultimatorException("Could not create UiDevice", e);
-		}
-		catch (InterruptedException e)
-		{
-			throw new UiMultimatorException("Could not create UiDevice", e);
-		}	
+		runner.run("se.vidstige.android.uimultimator.UiDeviceCommands", methodname);	
 	}
 }
